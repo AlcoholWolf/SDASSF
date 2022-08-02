f7bf892d02368521eff546ae4361996c0094d01f
늑이 <102508669+AlcoholWolf@users.noreply.github.com>
2022-08-02 11:21:49 +0900

    Update Other.py

--- Setting/Other.py
+++ Setting/Other.py
@@ -1,4 +1,3 @@
-plt.style.use('dark_background')
 import warnings
 warnings.filterwarnings(action='ignore')
-print('Bot | Setting Set Other : DarkBackground, Waring(Ignore)')
+print('Bot | Setting Set Other : Waring(Ignore)')
