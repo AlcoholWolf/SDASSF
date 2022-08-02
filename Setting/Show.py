a8ce70217a1caa1541a24c20b00532432805b03d
늑이 <102508669+AlcoholWolf@users.noreply.github.com>
2022-08-02 11:21:05 +0900

    Update Show.py

--- Setting/Show.py
+++ Setting/Show.py
@@ -3,4 +3,5 @@ import matplotlib.patches as mpatches
 import matplotlib as mpl
 import matplotlib.pylab as plb
 import seaborn as sns
-print('Bot | Setting Set Show : Seaborn(sns), Matplotlib(plt, mpatches, mpl, plb)')
+plt.style.use('dark_background')
+print('Bot | Setting Set Show : Seaborn(sns), Matplotlib(plt=dark_backgraund, mpatches, mpl, plb)')
