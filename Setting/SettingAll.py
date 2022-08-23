5c20b91e67695989e4b6bd95381d70aabf66374d
늑이 <102508669+AlcoholWolf@users.noreply.github.com>
2022-08-23 12:08:24 +0900

    Update SettingAll.py

--- Setting/SettingAll.py
+++ Setting/SettingAll.py
@@ -1,10 +1,24 @@
-from Other import *
-from AddOn import *
-from AddOnSklearn import *
-from Bagic import *
-from Calqulrus import *
-from DataLoader import *
-from Import import *
-from Show import *
-from System import *
-from os import *
+from tqdm import tqdm
+import time
+
+for x in tqdm(range(11)):
+    if x==1:
+        from Other import *
+    if x==2:
+        from AddOn import *
+    if x==3:
+        from AddOnSklearn import *
+    if x==4:
+        from Bagic import *
+    if x==5:
+        from Calqulrus import *
+    if x==6:
+        from DataLoader import *
+    if x==7:
+        from Import import *
+    if x==8:
+        from Show import *
+    if x==9:
+        from System import *
+    if x==10:
+        from os import *
